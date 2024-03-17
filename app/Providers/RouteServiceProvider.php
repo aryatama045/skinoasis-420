f18e496412437736a2df883c9877a59fb37816d3
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-17 09:18:25 +0700

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,34 +39,34 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
 
-            // Route::prefix('api')
-            //     ->middleware('api')
-            //     ->namespace($this->namespace)
-            //     ->group(base_path('routes/api.php'));
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
 
-            // Route::middleware('web')
-            //     ->namespace($this->namespace)
-            //     ->group(base_path('routes/admin.php'));
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
 
-            // Route::middleware('web')
-            // ->namespace($this->namespace)
-            // ->group(base_path('routes/halal.php'));
+            Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/halal.php'));
 
-            // Route::middleware('web')
-            // ->namespace($this->namespace)
-            // ->group(base_path('routes/deliveryman.php'));
+            Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/deliveryman.php'));
             
-            // Route::middleware('web')
-            //     ->namespace($this->namespace)
-            //     ->group(base_path('routes/web.php'));
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
 
             // Route::middleware('web')
             //     ->namespace($this->namespace)
             //     ->group(base_path('routes/setup.php')); 
 
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/update.php'));
+            // Route::middleware('web')
+            //     ->namespace($this->namespace)
+            //     ->group(base_path('routes/update.php'));
         });
     }
 

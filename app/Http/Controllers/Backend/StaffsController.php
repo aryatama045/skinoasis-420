7b721e5a4b4ee6f1cf38c5c7b9edff7789bddc41
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-18 12:53:30 +0700

    CRUD dokter, klinik, mitra

--- app/Http/Controllers/Backend/StaffsController.php
+++ app/Http/Controllers/Backend/StaffsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StaffRequestForm;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\City;
 use App\Models\SpatieRole;
 use Spatie\Permission\Models\Role;
 use Hash;
@@ -41,6 +42,68 @@ class StaffsController extends Controller
         return view('backend.pages.staffs.index', compact('staffs', 'searchKey'));
     }
 
+    # Dokter list
+    public function dokter(Request $request)
+    {
+        $searchKey = null;
+        $ownOrAllStaff = auth()->user()->can('own_staff') && auth()->user()->user_type != 'admin' ? true : false;
+        $dokters = User::where('user_type', 'dokter')->latest();
+        if ($request->search != null) {
+            $dokters = $dokters->where('name', 'like', '%' . $request->search . '%')
+                ->orWhere('email', 'like', '%' . $request->search . '%')
+                ->orWhere('phone', 'like', '%' . $request->search . '%');
+            $searchKey = $request->search;
+        }
+        if ($ownOrAllStaff) {
+
+            $dokters = $dokters->where('created_by', auth()->user()->id);
+        }
+        $dokters = $dokters->where('id', '!=', auth()->user()->id)->paginate(paginationNumber());
+        return view('backend.pages.staffs.dokter.index', compact('dokters', 'searchKey'));
+    }
+
+    # Klinik list
+    public function klinik(Request $request)
+    {
+        $searchKey = null;
+        $ownOrAllStaff = auth()->user()->can('own_staff') && auth()->user()->user_type != 'admin' ? true : false;
+        // $staffs = User::where('user_type', 'staff')->latest();
+        $kliniks = User::where('user_type', 'klinik')->latest();
+        if ($request->search != null) {
+            $kliniks = $kliniks->where('name', 'like', '%' . $request->search . '%')
+                ->orWhere('email', 'like', '%' . $request->search . '%')
+                ->orWhere('phone', 'like', '%' . $request->search . '%');
+            $searchKey = $request->search;
+        }
+        if ($ownOrAllStaff) {
+
+            $kliniks = $kliniks->where('created_by', auth()->user()->id);
+        }
+        $kliniks = $kliniks->where('id', '!=', auth()->user()->id)->paginate(paginationNumber());
+        return view('backend.pages.staffs.klinik.index', compact('kliniks', 'searchKey'));
+    }
+
+    # Mitra list
+    public function mitra(Request $request)
+    {
+        $searchKey = null;
+        $ownOrAllStaff = auth()->user()->can('own_staff') && auth()->user()->user_type != 'admin' ? true : false;
+        // $staffs = User::where('user_type', 'staff')->latest();
+        $mitras = User::where('user_type', 'mitra')->latest();
+        if ($request->search != null) {
+            $mitras = $mitras->where('name', 'like', '%' . $request->search . '%')
+                ->orWhere('email', 'like', '%' . $request->search . '%')
+                ->orWhere('phone', 'like', '%' . $request->search . '%');
+            $searchKey = $request->search;
+        }
+        if ($ownOrAllStaff) {
+
+            $mitras = $mitras->where('created_by', auth()->user()->id);
+        }
+        $mitras = $mitras->where('id', '!=', auth()->user()->id)->paginate(paginationNumber());
+        return view('backend.pages.staffs.mitra.index', compact('mitras', 'searchKey'));
+    }
+
     # return create form
     public function create()
     {
@@ -48,24 +111,71 @@ class StaffsController extends Controller
         return view('backend.pages.staffs.create', compact('roles'));
     }
 
+    # return create form
+    public function createmitra()
+    {
+        $roles = SpatieRole::oldest()->where('id', '=', 2)->isActive()->get();
+        $cities = City::get();
+        return view('backend.pages.staffs.mitra.create', compact('roles', 'cities'));
+    }
+
+    # return create form
+    public function createdokter()
+    {
+        $roles = SpatieRole::oldest()->where('id', '=', 3)->isActive()->get();
+        $cities = City::get();
+        return view('backend.pages.staffs.dokter.create', compact('roles', 'cities'));
+    }
+
+    # return create form
+    public function createklinik()
+    {
+        $roles = SpatieRole::oldest()->where('id', '=', 4)->isActive()->get();
+        $cities = City::get();
+        return view('backend.pages.staffs.klinik.create', compact('roles', 'cities'));
+    }
+
     # save new staff
     public function store(StaffRequestForm $request)
     {
+        if($request->role_id == 2){
+            $tipeuser = "mitra";
+        }else if($request->role_id == 3){
+            $tipeuser = "dokter";
+        }else if($request->role_id == 4){
+            $tipeuser = "klinik";
+        }else{
+            $tipeuser = "staff";
+        }
+
         if (User::where('email', $request->email)->first() == null) {
-            $user             = new User;
-            $user->name       = $request->name;
-            $user->email      = $request->email;
-            $user->shop_id    = auth()->user()->shop_id ?? 1;
-            $user->phone      = validatePhone($request->phone);
-            $user->user_type  = "staff";
-            $user->password   = Hash::make($request->password);
-            $user->role_id    = $request->role_id;
-            $user->created_by = auth()->user()->id;         
+            $user                   = new User;
+            $user->name             = $request->name;
+            $user->email            = $request->email;
+            $user->shop_id          = auth()->user()->shop_id;
+            $user->phone            = validatePhone($request->phone);
+            $user->user_type        = $tipeuser;
+            $user->password         = Hash::make($request->password);
+            $user->role_id          = $request->role_id;
+            $user->avatar           = $request->image;
+            $user->created_by       = auth()->user()->id;
+            $user->address          = $request->alamat;
+            $user->infolain          = $request->infolain;
+            $user->zona              = $request->zona;
             $user->save();
             $user->assignRole(SpatieRole::findOrFail($request->role_id)->name);
 
             flash(localize('Staff has been inserted successfully'))->success();
-            return redirect()->route('admin.staffs.index');
+
+            if($request->role_id == 1 || $request->role_id == 5){
+                return redirect()->route('admin.staffs.index');
+            }else if($request->role_id == 2){
+                return redirect()->route('admin.staffs.mitra');
+            }else if($request->role_id == 3){
+                return redirect()->route('admin.staffs.dokter');
+            }else if($request->role_id == 4){
+                return redirect()->route('admin.staffs.klinik');
+            }
         }
         flash(localize('Email already used'))->error();
         return back();
@@ -79,7 +189,31 @@ class StaffsController extends Controller
         return view('backend.pages.staffs.edit', compact('user', 'roles'));
     }
 
-    # update staff 
+    # edit mitra
+    public function editmitra($id)
+    {
+        $user  = User::findOrFail($id);
+        $roles = SpatieRole::oldest()->where('id', '=', 2)->isActive()->get();
+        return view('backend.pages.staffs.mitra.edit', compact('user', 'roles'));
+    }
+
+    # edit dokter
+    public function editdokter($id)
+    {
+        $user  = User::findOrFail($id);
+        $roles = SpatieRole::oldest()->where('id', '=', 3)->isActive()->get();
+        return view('backend.pages.staffs.dokter.edit', compact('user', 'roles'));
+    }
+
+    # edit klinik
+    public function editklinik($id)
+    {
+        $user  = User::findOrFail($id);
+        $roles = SpatieRole::oldest()->where('id', '=', 4)->isActive()->get();
+        return view('backend.pages.staffs.klinik.edit', compact('user', 'roles'));
+    }
+
+    # update staff
     public function update(Request $request)
     {
         $exit_email = User::where('email', $request->email)->where('id', '!=', $request->id)->first();
@@ -93,6 +227,8 @@ class StaffsController extends Controller
         $user->email      = $request->email;
         $user->phone      = validatePhone($request->phone);
         $user->role_id    = $request->role_id;
+
+        $user->infolain          = $request->infolain;
 
         if ($request->filled('role_id')) {
             $user->role_id    = $request->role_id;
@@ -112,14 +248,32 @@ class StaffsController extends Controller
             $user->assignRole(SpatieRole::findOrFail($request->role_id)->name);
         }
         flash(localize('Staff has been updated successfully'))->success();
-        return redirect()->route('admin.staffs.index');
+
+        if($request->role_id == 1 || $request->role_id == 5){
+            return redirect()->route('admin.staffs.index');
+        }else if($request->role_id == 2){
+            return redirect()->route('admin.staffs.mitra');
+        }else if($request->role_id == 3){
+            return redirect()->route('admin.staffs.dokter');
+        }else if($request->role_id == 4){
+            return redirect()->route('admin.staffs.klinik');
+        }
     }
 
-    # delete staff  
-    public function delete($id)
+    # delete staff
+    public function delete(Request $request ,$id)
     {
         User::where('id', $id)->forceDelete();
-        flash(localize('Staff has been deleted successfully'))->success();
-        return redirect()->route('admin.staffs.index');
+        flash(localize('Has been deleted successfully'))->success();
+
+        if($request->role_id == 1 || $request->role_id == 5){
+            return redirect()->route('admin.staffs.index');
+        }else if($request->role_id == 2){
+            return redirect()->route('admin.staffs.mitra');
+        }else if($request->role_id == 3){
+            return redirect()->route('admin.staffs.dokter');
+        }else if($request->role_id == 4){
+            return redirect()->route('admin.staffs.klinik');
+        }
     }
 }
---
+++ resources/views/backend/pages/staffs/mitra/create.blade.php
@@ -0,0 +1,157 @@
+@extends('backend.layouts.master')
+
+@section('title')
+{{ localize('Add New Employee Staff') }} {{ getSetting('title_separator') }} {{ getSetting('system_title') }}
+@endsection
+
+@section('contents')
+<section class="tt-section pt-4">
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col-12">
+                <div class="card tt-page-header">
+                    <div class="card-body d-lg-flex align-items-center justify-content-lg-between">
+                        <div class="tt-page-title">
+                            <h2 class="h5 mb-lg-0">{{ localize('Add New Person') }}</h2>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mb-4 g-4">
+
+            <!--left sidebar-->
+            <div class="col-xl-9 order-2 order-md-2 order-lg-2 order-xl-1">
+                <form action="{{ route('admin.staffs.store') }}" method="POST" class="pb-650">
+                    @csrf
+                    <!--basic information start-->
+                    <div class="card mb-4" id="section-1">
+                        <div class="card-body">
+                            <h5 class="mb-4">{{ localize('Basic Information') }}</h5>
+
+                            <div class="mb-4">
+                                <label for="name" class="form-label">Nama Lengkap<span class="text-danger ms-1">*</span></label>
+                                <input class="form-control" type="text" id="name" placeholder="Masukan Nama Lengkap" name="name" value="{{old('name')}}">
+                                @if ($errors->has('name'))
+                                <span class="text-danger">{{ $errors->first('name') }}</span>
+                                @endif
+                            </div>
+
+
+                            <div class="mb-4">
+                                <label for="email" class="form-label">Email<span class="text-danger ms-1">*</span></label>
+                                <input class="form-control" type="email" id="email" placeholder="Email" name="email" value="{{old('email')}}">
+                                @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                                @endif
+                            </div>
+
+
+                            <div class="mb-4">
+                                <label class="form-label">Role<span class="text-danger ms-1">*</span></label>
+                                <select class="select2 form-control" data-toggle="select2" name="role_id">
+                                    <option value="0">-- Pilih Role --</option>
+                                    @foreach ($roles as $role)
+                                    <option value="{{ $role->id }}">
+                                        {{ $role->name }}
+                                    </option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="mb-4">
+                                <label class="form-label">Zona (Hanya untuk Mitra/Klinik/Dokter)</label>
+                                <select class="select2 form-control" data-toggle="select2" name="zona">
+                                    <option value="0">-- Pilih Zona --</option>
+                                    @foreach ($cities as $kota)
+                                    <option value="{{ $kota->id }}">
+                                        {{ $kota->name }}
+                                    </option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="mb-4">
+                                <label for="phone" class="form-label">Nomor Handphone</label>
+                                <input class="form-control" type="number" id="phone" placeholder="contoh : 08123456789" name="phone" value="{{old('phone')}}">
+                            </div>
+
+                            <div class="mb-4">
+                                <label for="name" class="form-label">Alamat Lengkap<span class="text-danger ms-1">*</span></label>
+                                <textarea class="form-control" type="text" id="alamat" placeholder="Masukan Alamat Lengkap" name="alamat"></textarea>
+                            </div>
+
+                            <div class="mb-4">
+                                <label for="password" class="form-label">{{ localize('Password') }}<span class="text-danger ms-1">*</span></label>
+                                <input class="form-control" type="password" id="password" placeholder="{{ localize('Type password') }}" name="password">
+                                @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="mb-4">Informasi Lainnya</h5>
+
+                            <div class="mb-4">
+                                <label class="form-label">Foto</label>
+                                <div class="tt-image-drop rounded">
+                                    <span class="fw-semibold">Pilih Foto</span>
+                                    <!-- choose media -->
+                                    <div class="tt-product-thumb show-selected-files mt-3">
+                                        <div class="avatar avatar-xl cursor-pointer choose-media"
+                                            data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom"
+                                            onclick="showMediaManager(this)" data-selection="single">
+                                            <input type="hidden" name="image">
+                                            <div class="no-avatar rounded-circle">
+                                                <span><i data-feather="plus"></i></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- choose media -->
+                                </div>
+                            </div>
+
+                            <div class="mb-4">
+                                <label for="name" class="form-label">Deskripsi Mitra</label>
+                                <textarea class="" id="myTextarea" name="infolain"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <!--basic information end-->
+
+                    <!-- submit button -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="mb-4">
+                                <button class="btn btn-primary" type="submit">
+                                    <i data-feather="save" class="me-1"></i> {{ localize('Save Staff') }}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- submit button end -->
+
+                </form>
+            </div>
+
+            <!--right sidebar-->
+            <div class="col-xl-3 order-1 order-md-1 order-lg-1 order-xl-2">
+                <div class="card tt-sticky-sidebar d-none d-xl-block">
+                    <div class="card-body">
+                        <h5 class="mb-4">{{ localize('Staff Information') }}</h5>
+                        <div class="tt-vertical-step">
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="#section-1" class="active">{{ localize('Basic Information') }}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
